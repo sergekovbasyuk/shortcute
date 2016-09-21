fd025eb2b5284956b6740559d1a6c8567f5b1be0
sergekovbasyuk <kolbasovs@gmail.com>
2016-09-21 22:54:42 +0300

    router queryKey removed

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 import Case from '../casePage';
 import Layout from '../layout';
 import About from "../about";
@@ -7,10 +8,12 @@ import Contacts from "../contacts";
 import Main from "../main";
 import NotFound from '../NotFound';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false});
+
 export default React.createClass({
  render() {
    return (
-      <Router onUpdate={() => window.scrollTo(0, 0)} history={hashHistory}>
+      <Router onUpdate={() => window.scrollTo(0, 0)} history={appHistory}>
         <Route path='/' component={Layout}>
           <IndexRoute component={Main}></IndexRoute>
           <Route path='cases/:id' component={Case}></Route>
